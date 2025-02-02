fe215ebd0a5d0494f90878fb468b0857cc9d9439
Deepak Dass <iamdeepakdass@gmail.com>
2025-02-03 01:39:53 +0530

    User auth and testing

---
+++ app/api/auth/register/route.ts
@@ -0,0 +1,40 @@
+import { NextRequest, NextResponse } from "next/server";
+import { connectToDatabase } from "@/lib/db";
+import User from "@/models/User";
+import { error } from "console";
+
+export async function POST(request: NextRequest) {
+  try {
+    const { email, password } = await request.json();
+
+    if (!email || !password) {
+      return NextResponse.json(
+        { error: "Email and Password are required" },
+        { status: 400 }
+      );
+    }
+
+    await connectToDatabase();
+
+    const existingUser = await User.findOne({ email });
+
+    if (existingUser) {
+      return NextResponse.json(
+        { error: "Email already registered" },
+        { status: 400 }
+      );
+    }
+
+    await User.create({ email, password });
+
+    return NextResponse.json(
+      { message: "User successfully registered" },
+      { status: 201 }
+    );
+  } catch (error) {
+    return NextResponse.json(
+      { error: "Failed to register User" },
+      { status: 500 }
+    );
+  }
+}
